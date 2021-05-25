87560fba3e8f9a0c1d0ea7d3e990f6691bf3bdd6
Lianaburko <burkoliana@gmail.com>
2021-05-25 15:51:20 +0300

    Update test.cpp

--- Lab_7/test.cpp
+++ Lab_7/test.cpp
@@ -120,11 +120,6 @@ int main(){
 
     assert(dep1.get_doctors().size() == 2);
 
-    doc3.set_department(&dep2);
-    doc2.set_department(&dep2);
-
-    assert(dep2.get_doctors().size() == 1);
-    assert(doc1.get_department() == "Heart department");
 
     return 0;
 }
